29afe23c6c69fea92b84762eb1505e27ee53fb75
saber <saber.pourrahimi.1999@gmail.com>
2020-02-25 18:12:32 +0330

    Updates

---
+++ precache-manifest.db57419024c6731330f8e11c9a16c9b8.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1751502a10cf8e75c9c7fce60bf4de3",
+    "url": "/Resume/index.html"
+  },
+  {
+    "revision": "b97b9947dc744607823d",
+    "url": "/Resume/static/css/main.9e9d5874.chunk.css"
+  },
+  {
+    "revision": "f86fa560696cdc536e60",
+    "url": "/Resume/static/js/0.59fa2689.chunk.js"
+  },
+  {
+    "revision": "0f5e6f1b5203b86ab1202ae62480dbf2",
+    "url": "/Resume/static/js/0.59fa2689.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a2493073d2a7ab13d56a",
+    "url": "/Resume/static/js/3.fa54eb23.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Resume/static/js/3.fa54eb23.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0f648d6a79482ed1fc98",
+    "url": "/Resume/static/js/4.eea83cd3.chunk.js"
+  },
+  {
+    "revision": "8177060bc6be214ca0e6",
+    "url": "/Resume/static/js/5.7e0370d2.chunk.js"
+  },
+  {
+    "revision": "a6763701fd822f495cfb",
+    "url": "/Resume/static/js/6.b086bc5e.chunk.js"
+  },
+  {
+    "revision": "3d2d88b7877918a326634499feeab80c",
+    "url": "/Resume/static/js/6.b086bc5e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b97b9947dc744607823d",
+    "url": "/Resume/static/js/main.06e64ac6.chunk.js"
+  },
+  {
+    "revision": "317ca9fadcfe2dc41564",
+    "url": "/Resume/static/js/runtime-main.950e3299.js"
+  },
+  {
+    "revision": "e59513c82578df5dafb6b8da951be9b6",
+    "url": "/Resume/static/media/IranianSans.e59513c8.ttf"
+  },
+  {
+    "revision": "c4a938c1d757aecbc17eec00259b9b18",
+    "url": "/Resume/static/media/card-challeng.c4a938c1.png"
+  },
+  {
+    "revision": "a33f7f3ad33f7ce6ef3bbca561960a39",
+    "url": "/Resume/static/media/gmail.a33f7f3a.png"
+  },
+  {
+    "revision": "44f8b289c69632d304b73af7f90b0a49",
+    "url": "/Resume/static/media/linkdin.44f8b289.png"
+  },
+  {
+    "revision": "83ecaddce4533c7ca551d05baa4e0954",
+    "url": "/Resume/static/media/massenger.83ecaddc.png"
+  }
+]);
