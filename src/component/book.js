2aae0627727f1dce26a33c7fbbcd34adb64efb9b
Saber Pourrahimi <saber.pourrahimi.1999@gmail.com>
2021-08-15 17:20:40 +0430

    add resizer commponent

--- src/component/book.js
+++ src/component/book.js
@@ -9,6 +9,8 @@ import p2 from '../images/card-challeng.png'
 import Button from '@material-ui/core/Button'
 import Navbar from './nav-bar/navbar'
 import { useSelector } from 'react-redux'
+import Draggable from 'react-draggable'
+import Resizer from './resizer/resizer'
 
 function Book () {
 
@@ -24,53 +26,55 @@ function Book () {
   }
 
   return (
-    <div className="main-container" style={{justifyContent: state === "small" && "flex-end", alignItems: state === "small" && "flex-end"}}>
-      {state !== "close" && (<Navbar type="RESIZE_BOOK" />)}
-      <div className='book' style={{display: state !== "minimize" && "none"}}>
-        <div className='book-light'>
-          <div className='book-text'>
-            <Lottie options={defaultOptions} height={400} width={400} />
-            <div><span className='subtitle'>Front-end Developer</span></div>
-            <div><span className='info'>February 25, 1999</span></div>
-            <div><span className='info'>Andishe St., Beheshti St., Tehran, I.R. Iran</span></div>
-            <div><span className='info'>+98 (912) 956 8749</span></div>
-            <div>
-              <a href='mailto:saber.pourrahimi.1999@gmail.com'>
-                <img src={gmail} alt="gmail" />
-              </a>
-              <a href='https://github.com/spr021'>
-                <img src={github} alt="github" />
-              </a>
-              <a href='https://www.linkedin.com/in/saber-pourrahimi-534795192'>
-                <img src={linkdin} alt="linkdin" />
-              </a>
-            </div>
-            <div className='container'>
-              <img src={p1} alt='Avocado' className='image' />
-              <div className='middle'>
-                <span className='infoo'>this is a massneger but the server is down and i get a scren shoot but you can see real project</span>
-                <a target='blank' href='https://spr021.github.io/avocado/'>
-                  <Button variant='contained' color='primary'>
-                    Viste
-                  </Button>
-                </a>
+    <Draggable handle="span" bounds="parent">
+        <div className="main-container" style={{justifyContent: state === "small" && "flex-end", alignItems: state === "small" && "flex-end"}}>
+        <Resizer>
+          <span>{state !== "close" && (<Navbar type="RESIZE_BOOK" />)}</span>
+          <div className='book' style={{display: state !== "minimize" && "none"}}>
+              <div className='book-text'>
+                <Lottie options={defaultOptions} height="auto" width="auto" />
+                <div><span className='subtitle'>Front-end Developer</span></div>
+                <div><span className='info'>February 25, 1999</span></div>
+                <div><span className='info'>Andishe St., Beheshti St., Tehran, I.R. Iran</span></div>
+                <div><span className='info'>+98 (912) 956 8749</span></div>
+                <div>
+                  <a href='mailto:saber.pourrahimi.1999@gmail.com'>
+                    <img src={gmail} alt="gmail" />
+                  </a>
+                  <a href='https://github.com/spr021'>
+                    <img src={github} alt="github" />
+                  </a>
+                  <a href='https://www.linkedin.com/in/saber-pourrahimi-534795192'>
+                    <img src={linkdin} alt="linkdin" />
+                  </a>
+                </div>
+                <div className='container'>
+                  <img src={p1} alt='Avocado' className='image' />
+                  <div className='middle'>
+                    <span className='infoo'>this is a massneger but the server is down and i get a scren shoot but you can see real project</span>
+                    <a target='blank' href='https://spr021.github.io/avocado/'>
+                      <Button variant='contained' color='primary'>
+                        Viste
+                      </Button>
+                    </a>
+                  </div>
+                </div>
+                <div className='container'>
+                  <img src={p2} alt='Avocado' className='image' />
+                  <div className='middle'>
+                    <span className='infoo'>this is card challeg from ponisha company i like this :D</span>
+                    <a target='blank' href='https://spr021.github.io/Card-Challenge/'>
+                      <Button variant='contained' color='primary'>
+                        Viste
+                      </Button>
+                    </a>
+                  </div>
+                </div>
               </div>
             </div>
-            <div className='container'>
-              <img src={p2} alt='Avocado' className='image' />
-              <div className='middle'>
-                <span className='infoo'>this is card challeg from ponisha company i like this :D</span>
-                <a target='blank' href='https://spr021.github.io/Card-Challenge/'>
-                  <Button variant='contained' color='primary'>
-                    Viste
-                  </Button>
-                </a>
-              </div>
-            </div>
-          </div>
+          </Resizer>
         </div>
-      </div>
-    </div>
+    </Draggable>
   )
 }
 
--- src/component/terminal.js
+++ src/component/terminal.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react'
+import Draggable from 'react-draggable'
 import { useSelector } from 'react-redux'
 import Typewriter from 'typewriter-effect'
 import Navbar from './nav-bar/navbar'
+import Resizer from './resizer/resizer'
 
 function Terminal () {
 
@@ -13,10 +15,12 @@ function Terminal () {
   setTimeout(() => { setEnter1(true) }, 3000)
   setTimeout(() => { setEnter2(true) }, 3100)
   setTimeout(() => { setEnter3(true) }, 3300)
-  
+
   return (
+    <Draggable handle="span" bounds="parent">
     <div className="main-container" style={{justifyContent: state === "small" && "flex-end", alignItems: state === "small" && "flex-end"}}>
-      {state !== "close" && (<Navbar type="RESIZE_TERMINAL" />)}
+      <Resizer>
+      <span>{state !== "close" && (<Navbar type="RESIZE_TERMINAL" />)}</span>
       <div className='terminal' style={{display: state !== "minimize" && "none"}}>
         <div className='terminal-text'>
           <div className='body'>
@@ -43,7 +47,9 @@ function Terminal () {
           </div>
         </div>
       </div>
+    </Resizer>
     </div>
+    </Draggable>
   )
 }
 
